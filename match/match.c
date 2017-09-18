a993b38594baf784140ad4198cd9a44518dbbae4
gquesnot <32057757+gquesnot@users.noreply.github.com>
2017-09-18 10:22:52 +0200

    dza

--- match/match.c
+++ match/match.c
@@ -9,6 +9,37 @@ int     ft_strlen(char *str)
     return (i);
 }
 
+char *ft_strnncpy(char *dest, char *src, int n)
+{
+    int i;
+
+    i = n;
+    while(src[i])
+    {
+        dest[i] = src[i];
+        i = i + 1;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+char *remove_star(char *s1)
+{
+    int i;
+
+    i = 0;
+    while(s1[i])
+    {
+        if (s1[i] == '*' && s1[i + 1] == '*')
+        {
+            ft_strnncpy(s1, s1 + 1, i);
+        }
+        else
+            i = i + 1;
+    }
+    return (s1);
+}
+
 void     ft_match2(char *s1, char *s2, int *i, int *j)
 {
     if (s2[*j] == '*' && s1[*i] != s2[*j + 1])
@@ -44,35 +75,4 @@ int     ft_match(char *s1, char *s2)
             return (0);
     }
     return (1);
-}
-
-char *ft_strnncpy(char *dest, char *src, int n)
-{
-    int i;
-
-    i = n;
-    while(src[i])
-    {
-        dest[i] = src[i];
-        i = i + 1;
-    }
-    dest[i] = '\0';
-    return (dest);
-}
-
-char *remove_star(char *s1)
-{
-    int i;
-
-    i = 0;
-    while(s1[i])
-    {
-        if (s1[i] == '*' && s1[i + 1] == '*')
-        {
-            ft_strnncpy(s1, s1 + 1, i);
-        }
-        else
-            i = i + 1;
-    }
-    return (s1);
 }
